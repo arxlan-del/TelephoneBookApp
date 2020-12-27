a7db1dee2757e419d7f72b45ccd29111196dea75
Özkan Arslan <stechnorsk59@gmail.com>
2020-12-28 01:32:04 +0300

    Add project files.

---
+++ TelephoneBook.DatabaseLogicLayer/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace TelephoneBook.DatabaseLogicLayer
+{
+    public class Class1
+    {
+    }
+}
---
+++ TelephoneBook.UI/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace TelephoneBook.UI
+{
+    public class Class1
+    {
+    }
+}
